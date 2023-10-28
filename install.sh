82d2f7510ba324732569cbc37bd2b961f9ea1831
Weslley-Stein <weslley.stein@gmail.com>
2023-10-28 01:31:31 -0300

    fix: k9s installation

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 echo "Going to root home..."
 cd /root
 echo "Installing essential packages..."
-yum install -y yum-utils vim curl git make
-echo "Installing Go Toolkit..."
-yum module install go-toolset -y
+yum install -y yum-utils vim curl git file procps-ng
+yum groupinstall 'Development Tools' -y
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)" && test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 echo "Installing Terraform..."
 yum -y install terraform
 echo "Installing Kubectl..."
 cd /opt/ && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 echo "Installing k9s..."
-cd /opt && git clone https://github.com/derailed/k9s && cd k9s && make build && ./execs/k9s
+#cd /opt && git clone https://github.com/derailed/k9s && cd k9s && make build && ./execs/k9s
