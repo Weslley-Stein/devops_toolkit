b2a85eddecb9744c881e15be7bd1dab55b954a0f
Weslley-Stein <weslley.stein@gmail.com>
2023-10-28 00:42:06 -0300

    fix: fixing install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /root
 echo "Installing essential packages..."
 yum install -y yum-utils vim curl git make
 echo "Installing Go Toolkit..."
-yum module install go-toolset
+yum module install go-toolset -y
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 echo "Installing Terraform..."
 yum -y install terraform
