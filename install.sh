6f2bfe631af555d77fa9eae770c6f7ee5a9a3ad6
Weslley-Stein <weslley.stein@gmail.com>
2023-10-28 01:37:59 -0300

    feat: docker push and k9s installation

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ yum -y install terraform
 echo "Installing Kubectl..."
 cd /opt/ && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 echo "Installing k9s..."
-#cd /opt && git clone https://github.com/derailed/k9s && cd k9s && make build && ./execs/k9s
+brew install derailed/k9s/k9s
